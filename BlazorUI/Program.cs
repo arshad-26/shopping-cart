c4c22ff3be845aee66331bb43d43b81040d72dd3
arshu18 <arshadarif186@gmail.com>
2023-03-31 00:27:47 +0530

    Removed custom alerts and configured mudblazor

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,11 +1,15 @@
 using BlazorUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Scoped Services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:38451/api/") });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
