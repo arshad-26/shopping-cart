38a8015626475e667a437df8f8ea41c0e24bfc14
arshu18 <arshadarif186@gmail.com>
2023-04-03 01:59:57 +0530

    Global config for Snackbar

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -10,6 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Scoped Services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:38451/api/") });
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.ClearAfterNavigation = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+});
 
 await builder.Build().RunAsync();
