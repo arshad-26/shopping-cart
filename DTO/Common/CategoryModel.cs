b8482fe73f37c600335c0f5a412df96be3b456a3
Arshad Arif <Arshad.Arif@ingrammicro.com>
2023-06-04 23:30:27 +0530

    Added Category Popup

---
+++ DTO/Common/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Common;
+
+public class CategoryModel
+{
+    [Required(ErrorMessage = "This field is required")]
+    public string Category { get; set; } = default!;
+}
